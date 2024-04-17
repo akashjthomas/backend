32b8465311c9054e964efb40a6a069ca984a0803
akashjthomas <118128633+akashjthomas@users.noreply.github.com>
2024-04-17 11:26:30 +0530

    Update server.js

--- motor_server/server.js
+++ motor_server/server.js
@@ -28,13 +28,14 @@ app.use(express.static('public'));
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/elmotors', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://akashthomas33:SkyWalker%4043@cluster0.a7jhjwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
     })
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error);
     });
+
 
 const getalluser=require('./controllers/getallusers');
 app.use('/api/getalluser',getalluser);
